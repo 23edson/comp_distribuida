511e3447b7d96e742172baf2822b2a9ce29e28b2
Edson L <ed_edsoon@hotmail.com>
2016-03-21 15:27:53 -0300

    commit 1

---
+++ t1-part1/chat.py
@@ -0,0 +1,24 @@
+from bottle import run, get, post, view, request, redirect, response
+
+messages = [("Nobody", "Hello!")]
+
+@get('/')
+@view('index')
+def index():
+    return {'name': 'Nobody', 'messages': messages }
+
+@get('/<name>')
+@view('index')
+def index(name):
+    return {'name': name, 'messages': messages}
+
+@post('/send')
+def sendMessage():
+    m = request.forms.get('message')
+    n = request.forms.get('name')
+    
+    messages.append([n, m])
+    redirect('/'+n)
+
+
+run(host='localhost', port=8080)
