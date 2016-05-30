69ab76b325b13d586fca731e7a813f7941d7d220
Edson L <ed_edsoon@hotmail.com>
2016-05-30 20:36:10 -0300

    mm

--- final/vectorclock.py
+++ final/vectorclock.py
@@ -5,17 +5,45 @@ class VectorClock(object):
         self.clock = {}  # node => counter
         
     def getVector(self):
+    	mi = '/0'
     	m = []
-    		a = "%s"
-    		for node in sorted(self.clock.keys())])
-    		#m.append[('%s' % node,'%d' % self.clock[node])]
+    	mout = []
+    	mi = str(["%s:%d" % (node, self.clock[node]) for node in sorted(self.clock.keys())])
+    	mi = mi + ';'
+    	tam1 = len(mi)
+    	m = mi.split(',')
+    	#print(m)
+    	#mi = aux.split(
+    	i = 0
+    	total = 0
+    	for node in m:
+    		aux = str(node)
+    		#print(aux)
+    		idx = aux.find(':')
+    		tam = len(aux)
+    		if i == 0:
+    			ia = aux[2:idx]
+    			ib = aux[idx+1:tam-1]
+    			#ib = int(ib)
+    			mout.append([ia,ib])
+    			i = i+ 1
+    			#total = total + tam
+    		elif ';' in aux:
+    			ia = aux[2:idx]
+    			ib = aux[idx+1:tam-3]
+    			mout.append([ia,ib])
+    		else:
+    			ia = aux[2:idx]
+    			ib = aux[idx+1:tam-1]
+    			mout.append([ia,ib])
     		
-    	return m
+    	#print(mout)
+    	return mout
     	
     def listToObj(self,m):
     	for(i,j)in m:
     		self.clock[i] = j
-        
+    		
     def getCounter(self,node):
     	if node in self.clock:
     		return self.clock[node]
