ec0f6fada62c87b5dcb91477fe29d0390beb9efd
Edson L <ed_edsoon@hotmail.com>
2016-07-06 22:57:30 -0300

    docs update

--- final/table.py
+++ final/table.py
@@ -1,5 +1,7 @@
 #Computação Distribuída
 #Edson Lemes da Silva
+#Esta aplicação representa uma tabela de dados distribuída, assim, é possível enviar e receber dados na tabela
+#através de peers cuja url é conhecida
 
 from bottle import run, get, post, view, request, redirect, response, route,put
 import json
@@ -19,16 +21,12 @@ layoutx = []
 layouty = []
 x = 0
 y = 0
-#eventCount = 1
-#messages_1 = []
 
-#aux = []
 mylink = None
 
 NO_ERROR = 200
 
 Vector = VectorClock()
-#msgsCounter = 1
 
 @get('/')
 @view('index')
@@ -39,33 +37,31 @@ def index():
 #@view('index')
 #def index(name):
 #    return {'messages': messages, 'layoutx' : layoutx, 'layouty' : layouty,'users' : servers_list}
-
+#Recebe a mansagem e sincroniza com a lista, atualizando também o vectorClock respectivo a cada posição da
+#tabela
 @post('/send')
 def sendMessage():
 	global x
 	global y
-    #name=""
 	m = []
 	for i in range(1,(x*y)+1):
 		m.append(request.forms.get('message' + str(i)))
 		
-	#print(m)
+	
 	
 	for (indexx,i) in enumerate(m):
 		for (index,j) in enumerate(messages):
 			if indexx == index:
-				#print(str(indexx) +" " + str(index))
+				
 				j[0] = i
 				aux = Vector.getCounter(str(index))
 				aux = aux + 1
 				Vector.update(str(index), aux)
 			
-				#print(j)
+				
 				break
 				
-	#print(messages)
-   # messages.append([n, m])
-    #messages_1.append([n,m])
+	
 	redirect('/')
 
 @get('/peers')
@@ -90,15 +86,13 @@ def peersSyncMethod(name):
 	
 def getPeers(who):
     url = str(who[0]) + "/peers"
-    #print("teste"+url+"\n")
+    
 
     try:
  
         req = requests.get(url)
         if req.status_code == NO_ERROR:
             data = json.loads(req.text)
-            #print(data)
-            
             return data
     except:
        time.sleep(1) 
@@ -108,8 +102,7 @@ def getPeers(who):
         
 def getMessages(who):
     url = str(who[0]) + "/msgs"
-    #print(" " + url + "\n")
-   # print(url)
+    
     try:
 
         req = requests.get(url)
@@ -128,13 +121,11 @@ def putPeers(name):
 	host = host+str(name)
 	
 	flag = 1
-	#m = []
-	#m.append([hst,1])
+	
 	time.sleep(2)
 	#mutex.acquire(1)
 	for i in servers_list:
 		if i[0] == host:
-			#print(i[0]+ " "+ host)
 			flag = 0
 			break
 	if flag == 1:
@@ -156,15 +147,14 @@ def getClock(who):
 		time.sleep(1)
 	return None
 
-
+#Quando recebe a lista de mensagens de algum servidor, verifica no vectorCLock do servidor recebido se
+#há modificações mais recentes que a armazenada no vectorCLock local 
 def checkList(msg, host):
 	temp = VectorClock()
 	data = getClock(host)
 	for  (i,j) in data:
 		temp.update(i,j)
-	#temp.listToObj(data)
-	#print(msg)
-	#return 0
+	
 	for (index,msgi) in enumerate(msg):
 		aux = Vector.getCounter(str(index))
 		
@@ -178,7 +168,7 @@ def checkList(msg, host):
 		for (i,mes) in enumerate(messages):
 			if i == index:
 				mes[0] = msgi[0]
-				#print(str(i) + " " + str(mes[0]))
+				
     			
 
 def peersSynct(thread_name,mutex):
@@ -204,31 +194,30 @@ def serversControl(thread_name,mutex):
     	flag = 1
     	flag1 = 1
     	for links in servers_list:
-    		#print(links[0])
+    		
     		if links[0]!=myLink:
     	
     			new_peer=getPeers(links)
     			
     			if new_peer!=None:
-    				#getOnlineStatus(links[0],1)
+    				
     				links[1] = 1
     				for test in new_peer:
-    					#print("t"+str(test))
-    					#print("teste"+str(new_peer))
+    					
     					for aux in servers_list:
     						if test[0] == aux[0]:
     							flag = 0
     						
     					if flag == 1:
-    						print("omk")
+    						
     						servers_list.append(test)
     					else:
     						flag = 1
     			else:
-    				#getOnlineStatus(links[0],0)
+    				
     				links[1] = 0
     			
-    				#print("prob")
+    				
     	mutex.release()
         #  time.sleep(1)
 
@@ -236,7 +225,7 @@ def serversControl(thread_name,mutex):
 def messagesControl(thread_name,mutex):
     print(thread_name + " iniciada")
     while 1:
-        #print("f")
+        
         mutex.acquire(1)
         time.sleep(1.3)
         global messages
@@ -244,11 +233,13 @@ def messagesControl(thread_name,mutex):
             if link[0] != myLink:
                 msg = getMessages(link)
                 if msg != None:
-                    #print("entrei")
-                    #print(msg)
-                    checkList(msg, link[0])
+                	checkList(msg, link[0])
         mutex.release()
-        
+
+
+#A tabela é definida na lista messages seguindo o seguinte formato:
+#se há uma tabela 2x2, então a lista ficará [("","1,1;"),("","1,2;"),("","2,1;"),("","2,2;")].
+#Deste modo, cada cédula da tabela é identificada pelo segunda dado das tuplas na lista.        
 def getTableSize(x,y):
 	layoutx.append("")
 	count = 0
@@ -275,24 +266,17 @@ def getTableSize(x,y):
 x = int(sys.argv[3])
 y = int(sys.argv[2])
 getTableSize(x,y)
-#print(layoutx)
-#print(layouty)
-#print(messages)
+
 myLink = "http://localhost:" + str(sys.argv[1])
 servers_list.append([myLink,2])
-#know_servers.append([myLink])
+
 
 for i in sys.argv:
     if i != 'table.py' and i != sys.argv[1] and i!=sys.argv[2] and i!=sys.argv[3]:
         servers_list.append(["http://localhost:" +str(i),0])
-        #know_servers.append(["http://localhost:" +str(i)])
-        #msg = ("http://localhost:"+str(i),0)
-        #aux.append(msg)
+       
 print("lista de conhecidos inicializada\n")
 
-print(servers_list)
-
-    
 mutex = Lock()
 thread1 = Thread(target = serversControl, args = ("Thread 1",mutex)).start()
 thread2 = Thread(target = messagesControl, args = ("Thread 2",mutex)).start()
